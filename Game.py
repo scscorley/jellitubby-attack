77f1bc2423b2e18648982514967cc25c6f30148b
Carter Noordsij <carter.noordsij@kearsarge.org>
2013-11-13 08:27:55 -0500

    Changed Player.py to Vacuum.py and added game code

---
+++ Game.py
@@ -0,0 +1,106 @@
+import pygame, sys, math, random
+
+pygame.init()
+
+from Ball import Ball
+from PlayerBall import PlayerBall
+
+clock = pygame.time.Clock()
+
+width = 640
+height = 480
+size = width, height
+
+screen = pygame.display.set_mode(size)
+
+bgImage = pygame.image.load("")
+bgRect = bgImage.get_rect()
+
+bgColor = r,g,b = 0,0,0
+
+vacuum = PlayerBall([""], [3,3], [50,50], [width/2,height/2])
+
+monSize = random.randint(25, 150)
+monsters = [monster("", 
+              [random.randint(-5,5), random.randint(-5,5)], 
+              [bsize, bsize], 
+              [random.randint(75, width-75), random.randint(75, height-75)])]
+              
+start = False
+while True:
+    while not start:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    start = True
+        
+        screen.blit(bgImage, bgRect)
+        pygame.display.flip()
+        clock.tick(60)
+    
+    bgImage = pygame.image.load("")
+    
+    level = 1
+    
+    while start:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                    ballp.direction("right")
+                if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                    ballp.direction("left")
+                if event.key == pygame.K_w or event.key == pygame.K_UP:
+                    ballp.direction("up")
+                if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                    ballp.direction("down")
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                    ballp.direction("stop right")
+                if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                    ballp.direction("stop left")
+                if event.key == pygame.K_w or event.key == pygame.K_UP:
+                    ballp.direction("stop up")
+                if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                    ballp.direction("stop down")
+
+        for monster in monsters:
+            monster.update()
+        vacuum.update()        
+        
+        for monster in monsters:
+            monster.collideWall(width, height)
+        vacuum.collideWall(width, height)
+        
+        if len(monsters) > 1:
+            for first in range(len(monsters)-1):
+                for second in range(first+1,len(monsters)):
+                    monsters[first].collideBall(monsters[second])
+        
+        for monster in monsters:
+            vacuum.collideBall(monster)
+            
+        for monster in monsters:
+            if not monster.living:
+                monsters.remove(monster)
+                
+        
+        if len(monsters) == 0:
+            level += 1
+            for i in range(level):
+                print i
+                monSize = random.randint(25, 150)
+                monsters += [Monster("rsc/enemy/ball.png", 
+                              [random.randint(-5,5), random.randint(-5,5)], 
+                              [monSize, monSize], 
+                              [random.randint(75, width-75), random.randint(75, height-75)])]
+                          
+        screen.blit(bgImage, bgRect)
+        screen.blit(vacuum.image, vacuum.rect)
+        for monster in monsters:
+            screen.blit(monster.image, monster.rect)
+        pygame.display.flip()
+        clock.tick(60)
---
+++ Vacuum.py
@@ -0,0 +1,98 @@
+import pygame, sys, math
+
+class Vacuum():
+    def __init__(self, images, speed = [2,2], size = [100,100], pos = (0,0)):
+        self.images = []
+        for image in images:
+            newimage = pygame.image.load(image)
+            newimage = pygame.transform.scale(newimage, size)
+            self.images += [newimage]
+        self.frame = 0
+        self.maxFrame = len(self.images)-1
+        self.waitCount = 0
+        self.waitMax = 10
+        self.image = self.images[self.frame]
+        self.rect = self.image.get_rect()
+        self.maxSpeedx = speed[0]
+        self.maxSpeedy = speed[1]
+        self.speedx = 0
+        self.speedy = 0
+        self.speed = [self.speedx, self.speedy]
+        self.radius = self.rect.width/2
+        self.place(pos)
+        
+    def place(self, pos):
+        self.rect.center = pos
+        
+    def direction(self, dir):
+        if dir == "right":
+            self.speedx = self.maxSpeedx
+        if dir == "stop right":
+            self.speedx = 0
+        if dir == "left":
+            self.speedx = -self.maxSpeedx
+        if dir == "stop left":
+            self.speedx = 0
+        if dir == "up":
+            self.speedy = -self.maxSpeedy
+        if dir == "stop up":
+            self.speedy = 0
+        if dir == "down":
+            self.speedy = self.maxSpeedy
+        if dir == "stop down":
+            self.speedy = 0
+    
+    def update(self):
+        self.move()
+        self.animate()
+    
+    def animate(self):
+        if self.waitCount < self.waitMax:
+            self.waitCount += 1
+        else:
+            self.waitCount = 0
+            if self.frame < self.maxFrame:
+                self.frame += 1
+            else:
+                self.frame = 0
+            self.image = self.images[self.frame]
+    
+    def move(self):
+        self.speed = [self.speedx, self.speedy]
+        self.rect = self.rect.move(self.speed)
+        
+    def collideWall(self, width, height):
+        if self.rect.left < 0 or self.rect.right > width:
+            self.speedx = 0
+        if self.rect.top < 0 or self.rect.bottom > height:
+            self.speedy = 0
+            
+    def collideBall(self, other):
+        if self.rect.right > other.rect.left and self.rect.left < other.rect.right:
+            if self.rect.bottom > other.rect.top and self.rect.top < other.rect.bottom:
+                if self.radius + other.radius > self.distanceToPoint(other.rect.center):
+                    other.living = False
+                    if self.rect.center[0] < other.rect.center[0]: #self left of other
+                        if other.speedx < 0: #moving left
+                            other.speedx = -other.speedx
+                            other.didBounce = True
+                    if self.rect.center[0] > other.rect.center[0]: #self right of other
+                        if other.speedx > 0: #moving right
+                            other.speedx = -other.speedx
+                            other.didBounce = True
+                    if self.rect.center[1] < other.rect.center[1]: #self above other
+                        if other.speedy < 0: #moving up
+                            other.speedy = -other.speedy
+                            other.didBounce = True
+                    if self.rect.center[1] > other.rect.center[1]:#self below other
+                        if other.speedy > 0: #moving down
+                            other.speedy = -other.speedy
+                            other.didBounce = True
+    
+    def distanceToPoint(self, pt):
+        x1 = self.rect.center[0]
+        y1 = self.rect.center[1]
+        x2 = pt[0]
+        y2 = pt[1]
+        
+        return math.sqrt(((x2-x1)**2)+((y2-y1)**2))
